af4ae820131063958867a60c74ffb89bca6c8feb
nary-lev <marylevchenko@gmail.com>
2023-12-30 23:45:38 +0100

    Add draft description

--- pages/dataset.py
+++ pages/dataset.py
@@ -7,16 +7,26 @@ show_pages_from_config()
 
 st.write("""
         ## Original dataset and mushup datasets
-         Some text here
+        The datasets are characterized by their high-quality standards, following the technical specifications of WCAG 2.0. 
+        However, it’s noted that the site currently does not conform to all the accessibility standards outlined 
+         in EU Decision 2018/1523.
          
         ## Quality analysis of the datasets
+        The data within Immigrants.Stat are presented in multidimensional tables, allowing for a high degree of customization 
+         and analysis. The use of standard metadata ensures ease of access and understanding for users.
          
         ## Legal analysis (privacy, license, purpose, etc.)
+        The platform is openly accessible to the public and free of charge, reflecting its compliance with open 
+         data principles and ensuring broad usability.
          
         ## Ethics anlysis
         
         ## Technical analysis (formats, metadati, URI, provenance)
+        The data warehouse employs OECD.Stat technology, similar to the I.Stat platform used by Istat. 
+         This technology allows for advanced data organization, retrieval, and analysis.
         
         ## Sostenibility of the update the datasets over the time
+        The ongoing development of Immigrants.Stat implies a commitment to continuous updates and additions, 
+         ensuring the platform remains relevant and reflective of current trends and data.
         """
 )
--- pages/introduction.py
+++ pages/introduction.py
@@ -7,5 +7,8 @@ show_pages_from_config()
 
 st.write("""
         ## About
+        "Immigrati.Stat" is a comprehensive data warehouse developed by Istat, focusing on the statistical data about foreign immigrants and new citizens in Italy. It aims to streamline access to various statistics for a wide array of users including researchers, policymakers, journalists, and citizens.
+         
+         The "Immigrati.Stat" project was initiated on 22nd July 2012 with a mission to collate and organize diverse statistics related to foreign immigrants in Italy, offering a holistic view of the phenomenon. It encompasses several key areas such as population, health, labor, education, economic conditions, social security, welfare, social participation, and crime.
         """
 )

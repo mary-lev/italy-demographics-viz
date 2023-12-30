c1374c2793ab0c9aa30604f4e1a364bf7bb396bd
nary-lev <marylevchenko@gmail.com>
2023-12-31 00:18:09 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# Provincial Demographics: The Migrant Ratio
+
+This project is a Streamlit application that visualizes the demographics of provinces, focusing on the migrant ratio.
+
+## Installation
+
+Before running the application, you need to install the required Python packages. You can do this by running:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+To run the application, use the following command:
+
+```bash
+streamlit run app.py
+```
+
+## Features
+
+- **Provincial Demographics Visualization**: The application visualizes the demographics of provinces, with a focus on the migrant ratio. The color of each province is determined by its migrant ratio, with lower percentages starting at white and higher percentages at dark blue.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
--- pages/lod.py
+++ pages/lod.py
@@ -1,11 +1,57 @@
 import streamlit as st
 from st_pages import show_pages_from_config, add_page_title
 
-add_page_title()
-
 show_pages_from_config()
 
 st.write("""
-        ## About
+        ## Metadata for STRASA 2022 Provincial Data: Resident Foreigners
+        ### 1. Dataset Overview
+        Dataset Name: Resident Foreigners on 1st January 2022 by Age and Sex
+        Description: This dataset provides demographic information about the foreign resident population in various Italian provinces as of 1st January 2022, detailing age and gender distribution.
+        Format: CSV (Comma-Separated Values)
+        Data Collection Period: As of 1st January 2022
+        Source: Italian National Institute of Statistics (Istat)
+
+        ### 2. Column Descriptions
+
+        Province_Code: The code assigned to each province for identification purposes.
+        Province: The name of the province.
+        Age: Age of the individuals (likely categorized in groups or as a specific age).
+        Males: Number of male foreign residents in the province.
+        Females: Number of female foreign residents in the province.
+
+        ### 3. Data Quality and Limitations
+
+        Data accuracy is subject to Istat's reporting standards.
+        Age categorization method not specified (individual years, groups, or ranges).
+        Data specific to foreign residents only; does not include local citizens.
+
+        ### 4. Usage Rights and Licensing
+
+        Open for public use, subject to Istat's data distribution policies.
+
+        ### 5. Update Frequency
+
+        Annually
+
+        ### 6. Accessibility and Format
+
+        Accessible for download in CSV format.
+
+        ### 7. Contact Information
+
+        For inquiries and further information, contact michele.camisasca@istat.it.
+
+        ### 8. Technical Requirements
+
+        Compatible with standard data analysis software capable of processing CSV files.
+
+        ### 9. Supplementary Materials
+
+        None specified.
+
+        ### 10. Additional Notes
+
+        Ideal for demographic studies, migration research, social policy analysis, and resource allocation planning for foreign residents.
         """
 )
